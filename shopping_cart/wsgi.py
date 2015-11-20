d544ec4a5befe27c69e39b57601681c2cc327d60
Rushikesh <rushikesh.jadhav@wwindia.com>
2015-11-20 16:04:05 +0530

    Summary: Added some settings regarding  DjangoWhiteNoise in wsgi.py

--- shopping_cart/wsgi.py
+++ shopping_cart/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shopping_cart.settings")
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)

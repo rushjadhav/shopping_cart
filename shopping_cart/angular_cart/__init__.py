26b03049a28afa3f7941591ba1e49171a079a891
Rushikesh <rushikesh.jadhav@wwindia.com>
2015-11-20 13:05:40 +0530

    Summary: Changed folder structure of project.

--- shopping_cart/angular_cart/__init__.py
+++
@@ -1,2 +0,0 @@
-# in yourapp/__init__.py
-default_app_config = 'angular_cart.apps.CartAppConfig'
--- shopping_cart/angular_cart/apps.py
+++
@@ -1,6 +0,0 @@
-# in yourapp/apps.py
-from django.apps import AppConfig
-
-class CartAppConfig(AppConfig):
-    name = 'angular_cart'
-    verbose_name = 'Shopping Cart'

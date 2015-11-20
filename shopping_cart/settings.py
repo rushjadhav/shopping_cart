051cf15b0974476aee7941054100fb7e9b2f951f
Rushikesh <rushikesh.jadhav@wwindia.com>
2015-11-20 16:59:32 +0530

    Summary: Modified MEDIA_ROOT in settings

--- shopping_cart/settings.py
+++ shopping_cart/settings.py
@@ -95,7 +95,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 # Parse database configuration from $DATABASE_URL

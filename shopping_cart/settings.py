7eedcb40c4b36c4c8f0334ce1dfe76255e1d97d4
Rushikesh <rushikesh.jadhav@wwindia.com>
2015-11-20 15:55:59 +0530

    Summary: Changed settings

--- shopping_cart/settings.py
+++ shopping_cart/settings.py
@@ -118,6 +118,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
             os.path.join(BASE_DIR, 'static'),
+            os.path.join(BASE_DIR, '../angular_cart/static')
             )
 
 # Simplified static file serving.
